62448621ff10fa4c878a65fdca1bf33316238d54
VeryBigBraain <n.pozhidayev@mail.com>
2021-06-26 18:06:28 +0300

    adding todo

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
 const todosContainer = document.querySelector('.todo-container');
+const addTodoBtn = document.querySelector('.add-btn');
+const inputTodo = document.querySelector('.todo-input');
 const todos = [
     {id: 1, content: 'I am not gay', type: 'completed'},
     {id: 2, content: 'Misha is not gay(yes he is)', type: 'uncompleted'}
 ];
 
-function showTodos() {
-    todos.forEach(todo => {
-        todoNode = document.createElement('div');
-        todoNode.classList.add('todo__item', todo.type);
-        todoNode.id = todo.id;  
+function showTodos(todo) {
+    todoNode = document.createElement('div');
+    todoNode.classList.add('todo__item', todo.type);
+    todoNode.id = todo.id;  
 
-        todoContent = document.createElement('li');
-        todoContent.classList.add('todo__content');
-        todoContent.innerHTML = todo.content;
+    todoContent = document.createElement('li');
+    todoContent.classList.add('todo__content');
+    todoContent.innerHTML = todo.content;
 
-        completeBtn = document.createElement('button');
-        completeBtn.classList.add('complete-btn', 'btn');
-        completeIcon = document.createElement('i');
-        completeIcon.classList.add('gg-check-o');
-        completeBtn.appendChild(completeIcon);
-        
-        deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('delete-btn', 'btn');
-        deleteIcon = document.createElement('i');
-        deleteIcon.classList.add('gg-trash-empty');
-        deleteBtn.appendChild(deleteIcon);
-        
-        todoNode.appendChild(todoContent);
-        todoNode.appendChild(completeBtn);
-        todoNode.appendChild(deleteBtn);
+    completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete-btn', 'btn');
+    completeIcon = document.createElement('i');
+    completeIcon.classList.add('gg-check-o');
+    completeBtn.appendChild(completeIcon);
+    
+    deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn', 'btn');
+    deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('gg-trash-empty');
+    deleteBtn.appendChild(deleteIcon);
+    
+    todoNode.appendChild(todoContent);
+    todoNode.appendChild(completeBtn);
+    todoNode.appendChild(deleteBtn);
 
-        todosContainer.appendChild(todoNode);
-    });
+    todosContainer.appendChild(todoNode);
 };
 
-showTodos();
+function addTodo(e) {
+    e.preventDefault();
+
+    let now = new Date();
+    const todo = ({
+        id: now.getTime(),
+        content: inputTodo.value,
+        type: 'uncompleted'
+    });
+
+    inputTodo.value = '';
+    showTodos(todo);
+}
+
+todos.forEach(todo => showTodos(todo));
+
+addTodoBtn.addEventListener('click', addTodo);
