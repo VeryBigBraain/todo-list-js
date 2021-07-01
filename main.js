142a42f9fa58aae472607be87c42725a07af84c0
VeryBigBraain <n.pozhidayev@mail.com>
2021-07-01 15:38:54 +0300

    add js for adding todo

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const todosContainer = document.querySelector('.todo-container');
 const addTodoBtn = document.querySelector('.add-btn');
 const inputTodo = document.querySelector('.todo-input');
-const todos = [
-  { id: 1, content: 'bla bla bla', type: 'completed' },
-  { id: 2, content: 'Misha is not bla bla bla(yes he is)', type: 'uncompleted' },
-];
 
-function showTodos(todo) {
+function showTodo(todo) {
   const todoNode = document.createElement('div');
   todoNode.classList.add('todo__item', todo.type);
   todoNode.id = todo.id;
@@ -30,36 +26,19 @@ function showTodos(todo) {
 }
 function addTodo(e) {
   e.preventDefault();
-  const now = new Date();
-  const todo = ({
-    id: now.getTime(),
-    content: inputTodo.value,
-    type: 'uncompleted',
-  });
-  todos.push(todo);
-  inputTodo.value = '';
-  showTodos(todo);
-}
-function deleteCheck(e) {
-  const item = e.target;
-  // Delete
-  if (item.classList[0] === 'delete-btn') {
-    const todo = item.parentElement;
-    todos.filter((arrTodo) => todo.id !== arrTodo.id);
-    todo.remove();
+  if (inputTodo.value.trim() !== '') {
+    const now = new Date();
+    const todo = ({
+      id: now.getTime(),
+      content: inputTodo.value,
+      type: 'uncompleted',
+    });
+    inputTodo.value = '';
+    showTodo(todo);
+  } else {
+    return null;
   }
-  // Check mark
-  if (item.classList[0] === 'complete-btn') {
-    const todo = item.parentElement;
-    todo.classList.toggle('complete');
-    todos.forEach(((arrTodo) => {
-      if (todo.id === arrTodo.id) {
-        return { ...arrTodo, type: todo.classList[todo.classList.length] };
-      }
-      return arrTodo;
-    }));
-  }
+  return true;
 }
-todos.forEach((todo) => showTodos(todo));
+
 addTodoBtn.addEventListener('click', addTodo);
-todosContainer.addEventListener('click', deleteCheck);
